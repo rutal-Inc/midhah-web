36d247cce391fbd56faa2ffe81ce2dfa321b9c69
Akil Mehar <akilmehar12@gmail.com>
2022-10-29 23:23:50 +0500

    create navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
