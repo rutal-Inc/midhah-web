bb1d8982e79ebb09f09f8e7a2e063cb8c967fadf
Talha Meer <muhammadtalha007@outlook.com>
2022-10-30 10:45:40 +0500

    add jumbotron in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/Navbar";
+import Jumbotron from "../components/Jumbotron";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Jumbotron />
     </div>
   );
 };
