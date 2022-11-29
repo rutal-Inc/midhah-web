ca990ad66ca729fb6cfb230e88d60f79dee0b69e
Talha Meer <muhammadtalha007@outlook.com>
2022-11-29 23:42:09 +0500

    fix the prop class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,38 +7,38 @@ const Footer = () => {
   const year = today.getFullYear();
   return (
     <div>
-      <footer class="d-flex flex-wrap justify-content-between align-items-center mx-3 py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center mx-3 py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
           <a
             href="/"
-            class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+            className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
           ></a>
-          <span class="mb-3 mb-md-0 text-muted">© {year} Midhah</span>
+          <span className="mb-3 mb-md-0 text-muted">&copy; {year} Midhah</span>
         </div>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li class="ms-3">
-            <a class="text-muted" href="#"></a>
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3">
+            <a className="text-muted" href="#"></a>
           </li>
-          <li class="ms-3">
+          <li className="ms-3">
             <a
-              class="text-muted"
+              className="text-muted"
               href="https://www.instagram.com/midhah.official/"
             >
-              <i class="bi bi-instagram"></i>
+              <i className="bi bi-instagram"></i>
             </a>
           </li>
-          <li class="ms-3">
+          <li className="ms-3">
             <a
-              class="text-muted"
+              className="text-muted"
               href="https://www.facebook.com/midhah.official"
             >
-              <i class="bi bi-facebook"></i>
+              <i className="bi bi-facebook"></i>
             </a>
           </li>
-          <li class="ms-3">
-            <a class="text-muted" href="https://twitter.com/midhahOfficial">
-              <i class="bi bi-twitter"></i>
+          <li className="ms-3">
+            <a className="text-muted" href="https://twitter.com/midhahOfficial">
+              <i className="bi bi-twitter"></i>
             </a>
           </li>
         </ul>
