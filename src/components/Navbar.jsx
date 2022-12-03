8819d45d9bf67f69acad36d12efb4fc99cd26db3
Talha Meer <muhammadtalha007@outlook.com>
2022-12-03 11:48:57 +0500

    redesign the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,30 +7,9 @@ const Navbar = () => {
     <>
       <div className="container">
         <header className="d-flex flex-wrap align-items-center justify-content-center mb-4 border-bottom">
-          <a
-            href="/"
-            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
-          >
-            <img src={logo} height="70px" />
+          <a href="/">
+            <img src={logo} alt="Midhah Lyrics Logo" height="70px" />
           </a>
-
-          <ul className="nav nav-pills">
-            <li className="nav-item">
-              <a href="#" className="nav-link active" aria-current="page">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                Contact Us
-              </a>
-            </li>
-          </ul>
         </header>
       </div>
     </>
