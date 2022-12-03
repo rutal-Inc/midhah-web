add9dc33de0a446f3d0a89c0bc57384d0d3b4689
Talha Meer <muhammadtalha007@outlook.com>
2022-12-03 11:37:49 +0500

    add content in the header

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import GooglePlay from "../assets/Google_Play_Store_badge_EN.png";
+
 const Jumbotron = () => {
   return (
     <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-      <div className="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 className="display-4 fw-normal">Punny headline</h1>
-        <p className="lead fw-normal">
-          And an even wittier subheading to boot. Jumpstart your marketing
-          efforts with this example based on Apple’s marketing pages.
+      <div className="col-md-7 mx-auto my-5">
+        <h1 className="display-4 fw-normal">
+          Explore the most authentic lyrics
+        </h1>
+        <p className="col-md-9 mx-auto lead fw-normal">
+          Midhah مدحة is the leading and most authentic platform for Naat
+          lyrics, in addition to Hamd, Manqbat, and Durood o Salam
         </p>
-        <a className="btn btn-outline-secondary" href="#">
-          Coming soon
+        <a
+          href="https://play.google.com/store/apps/details?id=com.midhah.lyrics"
+          target="_blank"
+        >
+          <img src={GooglePlay} alt="Get it on Google Play" />
         </a>
       </div>
       <div className="product-device shadow-sm d-none d-md-block"></div>
