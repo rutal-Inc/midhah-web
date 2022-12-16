3ad75ca493b5a78e3147e1df762c3d2e25747a0c
Rana Muzamal <muzamilrana149@gmail.com>
2022-12-16 16:56:13 +0500

    Add Routing using React Router Dom and connect the Privacy policy Page by routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Jumbotron from "../components/Jumbotron";
 import GenreCards from "../components/GenreCards";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
       <Jumbotron />
       <GenreCards />
-      <Footer/>
     </div>
   );
 };
