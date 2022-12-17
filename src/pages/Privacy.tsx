a2dca73bd7b69b71c2a6141987f11691c92e1b64
Rana Muzamal <muzamilrana149@gmail.com>
2022-12-17 19:14:58 +0500

    The privacy policy content according to  Midhah Lyrics app

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -22,8 +22,8 @@ const Privacy = () => {
           <br />
           If you choose to use my Service, then you agree to the collection and
           use of information in relation to this policy. The Personal
-          Information that I collect is used for providing and improving the
-          Service. I will not use or share your information with anyone except
+          Information that we collect is used for providing and improving the
+          Service. We will not use or share your information with anyone except
           as described in this Privacy Policy.
           <br />
           The terms used in this Privacy Policy have the same meanings as in our
@@ -34,9 +34,9 @@ const Privacy = () => {
           <h3>Information Collection and Use</h3>
 
           <p>
-            For a better experience, while using our Service, I may require you
+            For a better experience, while using our Service, we may require you
             to provide us with certain personally identifiable information. The
-            information that I request will be retained on your device and is
+            information that we request will be retained on your device and is
             not collected by me in any way.
             <br />
             The app does use third-party services that may collect information
@@ -44,6 +44,9 @@ const Privacy = () => {
             <br />
             Link to the privacy policy of third-party service providers used by
             the app
+            <ul>
+              <li className="text-decoration-underline">Google Play Services</li>
+            </ul>
           </p>
         </div>
 
@@ -51,8 +54,8 @@ const Privacy = () => {
           <h3>Log Data</h3>
 
           <p>
-            I want to inform you that whenever you use my Service, in a case of
-            an error in the app I collect data and information (through
+            We want to inform you that whenever you use my Service, in a case of
+            an error in the app We collect data and information (through
             third-party products) on your phone called Log Data. This Log Data
             may include information such as your device Internet Protocol (
             <strong>IP</strong>) address, device name, operating system version,
@@ -80,7 +83,7 @@ const Privacy = () => {
         <div className="p-1">
           <h3>Service Providers</h3>
           <p>
-            I may employ third-party companies and individuals due to the
+            We may employ third-party companies and individuals due to the
             following reasons:
           </p>
           <ul>
@@ -90,7 +93,7 @@ const Privacy = () => {
             <li>To assist us in analyzing how our Service is used</li>
           </ul>
           <p>
-            I want to inform users of this Service that these third parties have
+            We want to inform users of this Service that these third parties have
             access to their Personal Information. The reason is to perform the
             tasks assigned to them on our behalf. However, they are obligated
             not to disclose or use the information for any other purpose.
@@ -100,10 +103,10 @@ const Privacy = () => {
         <div className="p-1">
           <h3>Security</h3>
           <p>
-            I value your trust in providing us your Personal Information, thus
+            We value your trust in providing us your Personal Information, thus
             we are striving to use commercially acceptable means of protecting
             it. But remember that no method of transmission over the internet,
-            or method of electronic storage is 100% secure and reliable, and I
+            or method of electronic storage is 100% secure and reliable, and we
             cannot guarantee its absolute security.
           </p>
         </div>
@@ -113,23 +116,10 @@ const Privacy = () => {
           <p>
             This Service may contain links to other sites. If you click on a
             third-party link, you will be directed to that site. Note that these
-            external sites are not operated by me. Therefore, I strongly advise
-            you to review the Privacy Policy of these websites. I have no
+            external sites are not operated by me. Therefore, We strongly advise
+            you to review the Privacy Policy of these websites. We have no
             control over and assume no responsibility for the content, privacy
             policies, or practices of any third-party sites or services.
-          </p>
-        </div>
-
-        <div className="p-1">
-          <h3>Children’s Privacy</h3>
-          <p>
-            These Services do not address anyone under the age of 13. I do not
-            knowingly collect personally identifiable information from children
-            under 13 years of age. In the case I discover that a child under 13
-            has provided me with personal information, I immediately delete this
-            from our servers. If you are a parent or guardian and you are aware
-            that your child has provided us with personal information, please
-            contact me so that I will be able to do the necessary actions.
           </p>
         </div>
 
@@ -137,7 +127,7 @@ const Privacy = () => {
           <h3>Changes to This Privacy Policy</h3>
           <p>
             We may update our Privacy Policy from time to time. Thus, you are
-            advised to review this page periodically for any changes. I will
+            advised to review this page periodically for any changes. We will
             notify you of any changes by posting the new Privacy Policy on this
             page.
             <br />
