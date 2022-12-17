49d3267fe8fa8b79e9489f0eea97fae4c1470264
Rana Muzamal <muzamilrana149@gmail.com>
2022-12-17 23:52:33 +0500

    Update the footer by setting logo and Current year Dynamically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,9 @@ const Footer = () => {
   return (
     <div>
       <footer className="container mx-auto py-3 my-4 border-top">
-        <div className="d-flex flex-wrap justify-content-between align-items-center">
-          <ul className="nav col-md-4 justify-content-start list-unstyled d-flex ">
-            <li className="ms-3">
+        <div className="row flex-wrap justify-content-between align-items-center">
+          <ul className="nav col-md-4 justify-content-center justify-content-md-start list-unstyled d-flex order-2 order-md-1 my-3">
+            <li>
               <a
                 className="text-muted"
                 href="https://www.facebook.com/midhah.official"
@@ -39,28 +39,25 @@ const Footer = () => {
             </li>
           </ul>
 
-          <div className="col-md-4 d-flex align-items-center">
-            <span className="mb-3 mb-md-0 text-muted">
-              <img
-                src={greylogo}
-                alt="Midhah Grey Logo"
-                style={{
-                  width: "18px",
-                  borderRadius: "5px",
-                  marginRight: "5px",
-                }}
-              />
-              &copy; {year} Midhah
-            </span>
+          <div className="col-md-4 d-flex align-items-center justify-content-center my-3 order-3 order-md-2">
+            <img
+              src={greylogo}
+              alt="Midhah Grey Logo"
+              style={{
+                width: "30px",
+                marginRight: "5px",
+              }}
+            />
+            <span className="mb-0 mx-1 text-muted">&copy; {year} Midhah</span>
           </div>
 
-          <div>
+          <div className="d-flex col-md-4 justify-content-center justify-content-md-end my-3 order-2 order-md-3">
             <a
               href="/privacy-policy"
-              className=" text-muted"
+              className="text-muted"
               style={{ textDecoration: "none" }}
             >
-              Privacy
+              Privacy Policy
             </a>
           </div>
         </div>
