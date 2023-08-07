0c798c28bec1f47fd0bb5e13240618fcc99e62b2
Talha Meer <muhammadtalha007@outlook.com>
2023-08-08 00:07:01 +0500

    set color theme for th app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const Color = require("color");
+
+const lighten = (color, val) =>
+  Color(color).mix(Color("white"), val).rgb().string();
+const darken = (color, val) =>
+  Color(color).mix(Color("black"), val).rgb().string();
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,8 +14,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: {
+          DEFAULT: "#256279",
+          light: lighten("#256279", 0.2),
+          dark: darken("#256279", 0.2),
+        },
+        secondary: {
+          DEFAULT: "#1C8EB0",
+          light: lighten("#1C8EB0", 0.2),
+          dark: darken("#1C8EB0", 0.2),
+        },
+        tertiary: {
+          DEFAULT: "#247C99",
+          light: lighten("#247C99", 0.2),
+          dark: darken("#247C99", 0.2),
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
