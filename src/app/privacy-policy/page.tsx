49be89ba79dba3b9b06a4307b62d9b723e02d3a0
Talha Meer <muhammadtalha007@outlook.com>
2023-11-08 11:44:25 +0500

    fix sonar warning

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -83,10 +83,10 @@ const Privacy = () => {
             We want to inform you that whenever you use my Service, in a case of
             an error in the app We collect data and information (through
             third-party products) on your phone called Log Data. This Log Data
-            may include information such as your device Internet Protocol (
-            <strong>IP</strong>) address, device name, operating system version,
-            the configuration of the app when utilizing my Service, the time and
-            date of your use of the Service, and other statistics.
+            may include information such as your device Internet Protocol (IP)
+            address, device name, operating system version, the configuration of
+            the app when utilizing my Service, the time and date of your use of
+            the Service, and other statistics.
           </p>
         </div>
         <div className=" mb-4 p-1 leading-relaxed">
