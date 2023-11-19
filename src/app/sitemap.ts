bd14a9f21fa912ea8ad9e4b1cd88c438e324d9e7
Talha Meer <muhammadtalha007@outlook.com>
2023-11-19 21:49:19 +0500

    hotfix sitemap.xml url issue

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,12 @@
 import { MetadataRoute } from "next";
 import { WEB_BASE_URL } from "../utilities/constants";
 
-type SitemapRes = { slug: string; updatedAt: Date };
+type SitemapRes = { genre: string; slug: string; updatedAt: Date };
 
 const getSitemap = async () => {
   const res = await fetch(`https://api.midhah.com/v2/sitemap`, {
     method: "GET",
+    cache: "no-store",
     headers: {
       "Content-Type": "application/json",
     },
@@ -20,34 +21,48 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const sitemapRes = await getSitemap();
 
   const sitemap: MetadataRoute.Sitemap = sitemapRes.map((sitemap) => ({
-    url: `${WEB_BASE_URL}/${sitemap.slug}`,
+    url: `${WEB_BASE_URL}/${sitemap.genre}/${sitemap.slug}`,
     lastModified: sitemap.updatedAt,
+    changeFrequency: "weekly",
+    priority: 1,
   }));
 
   return [
     {
       url: WEB_BASE_URL,
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
       url: `${WEB_BASE_URL}/hamd`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
-      url: `${WEB_BASE_URL}/naat`,
+      url: `${WEB_BASE_URL}/weekly`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
       url: `${WEB_BASE_URL}/manqbat`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
       url: `${WEB_BASE_URL}/durood-o-salam`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
       url: `${WEB_BASE_URL}/privacy-policy`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     ...sitemap,
   ];
