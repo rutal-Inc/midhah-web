e6873d91d6ae1935e7010b3898f7a10e65e454cd
Talha Meer <muhammadtalha007@outlook.com>
2023-11-23 01:12:05 +0500

    create feature cards

---
+++ src/components/FeatureCards.tsx
@@ -0,0 +1,113 @@
+import Image from "next/image";
+import downloadsScreen from "../assets/screenshots/downloads-screen.jpeg";
+import homeScreen from "../assets/screenshots/home-screen.jpeg";
+import listingScreen from "../assets/screenshots/listing-screen.jpeg";
+import lyricsScreen from "../assets/screenshots/lyrics-screen.jpeg";
+import searchScreen from "../assets/screenshots/search-screen.jpeg";
+
+export default function FeatureCards() {
+  return (
+    <div className="bg-slate-200">
+      <div className="mx-auto py-10 pt-10 2xl:container md:px-[70px] md:pt-14">
+        <h2 className="mb-1 pl-4 text-2xl md:mb-4 md:text-4xl">
+          Discover Uncharted Devotion
+        </h2>
+        <p className="text-normal mb-6 pl-4 md:text-xl ">
+          Dive into a world of authenticity, diversity, and immersive lyrical
+          experiences
+        </p>
+
+        <div className="m-3 grid grid-cols-1 gap-4 lg:grid-cols-2">
+          <div className="bg-slate-100 px-4 pt-4 ">
+            <div className="my-4 py-4 text-center">
+              <h3 className="mb-3 text-2xl md:text-4xl">Pure Poetic Purity</h3>
+              <p className="text-normal md:text-xl">
+                Ensuring authenticity—experience the genuine words of poets,
+                untouched and true
+              </p>
+            </div>
+            <div className="mx-auto h-80 max-w-[40%] overflow-hidden">
+              <Image
+                src={lyricsScreen}
+                alt="home screen"
+                className="aspect-[9/16] rounded-3xl border-8 border-y-[10px] border-gray-800"
+              />
+            </div>
+          </div>
+
+          <div className="bg-primary px-4 pt-4 ">
+            <div className="my-4 py-4 text-center text-white">
+              <h3 className="mb-3 text-2xl md:text-4xl">
+                {/* Explore Instantly */}
+                At Your Fingertips
+              </h3>
+              <p className="text-normal md:text-xl">
+                Unleash the power of precision with our state-of-the-art search
+                functionality
+                {/* Advanced Search for Hamd, Naat, Manqbat, and Durood o Salam */}
+              </p>
+            </div>
+            <div className="mx-auto h-80 max-w-[40%] overflow-hidden">
+              <Image
+                src={searchScreen}
+                alt="search screen"
+                className="aspect-[9/16] rounded-3xl border-8 border-y-[10px] border-gray-800"
+              />
+            </div>
+          </div>
+
+          <div className="bg-primary px-4 pt-4 ">
+            <div className="my-4 py-4 text-center text-white">
+              <h3 className="mb-3 text-2xl md:text-4xl">Offline Access</h3>
+              <p className="text-normal md:text-xl">
+                Capture the essence offline—download and access your favorite
+                verses anytime, anywhere
+              </p>
+            </div>
+            <div className="mx-auto h-80 max-w-[40%] overflow-hidden">
+              <Image
+                src={downloadsScreen}
+                alt="home screen"
+                className="aspect-[9/16] rounded-3xl border-8 border-y-[10px] border-gray-800"
+              />
+            </div>
+          </div>
+
+          <div className="bg-slate-300 px-4 pt-4">
+            <div className="my-4 py-4 text-center">
+              <h3 className="mb-3 text-2xl md:text-4xl">Lyrical Oasis</h3>
+              <p className="text-normal md:text-xl">
+                A vast collection spanning Hamd, Naat, Manqabat, and Durood o
+                Salam—thousands of lyrical gems await
+              </p>
+            </div>
+            <div className="mx-auto h-80 max-w-[60%] overflow-hidden sm:max-w-[50%] md:h-96 lg:h-80 xl:max-w-[40%]">
+              <Image
+                src={listingScreen}
+                alt="home screen"
+                className="aspect-[9/16] rounded-3xl border-8 border-y-[10px] border-gray-800"
+              />
+            </div>
+          </div>
+
+          <div className="bg-slate-100 px-4 pt-4 ">
+            <div className="my-4 py-4 text-center">
+              <h3 className="mb-3 text-2xl md:text-4xl">Interactive Harmony</h3>
+              <p className="text-normal md:text-xl">
+                Embark on a seamless lyrical journey with our user-friendly
+                interface
+              </p>
+            </div>
+            <div className="mx-auto h-80 overflow-hidden xl:max-w-[40%]">
+              <Image
+                src={homeScreen}
+                alt="home screen"
+                className="aspect-[9/16] rounded-3xl border-8 border-y-[10px] border-gray-800"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
