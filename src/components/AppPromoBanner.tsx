705e6e284707351557125c66c6794b53b8cb431f
Talha Meer <talha.shahid@khired.com>
2024-09-15 03:34:18 +0500

    fix font issue in app promo banner

--- src/components/AppPromoBanner.tsx
+++ src/components/AppPromoBanner.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { montserrat } from "../app/fonts";
 import { promoLines } from "../utilities/constants";
 import GooglePlayBadge from "./GooglePlayBadge";
 
@@ -21,7 +22,7 @@ export const AppPromoBanner = () => {
   return (
     <>
       {isVisible && (
-        <div className="flex justify-center">
+        <div className={`flex justify-center ${montserrat.className}`}>
           <br />
           <div className="hero-bg relative mx-2 my-6 flex w-full flex-col items-center justify-between gap-2 rounded-lg p-5 text-white shadow-lg md:mx-auto md:w-3/4 md:flex-row md:gap-10 lg:w-3/5 lg:p-7">
             <p className="flex items-center text-left text-lg font-medium md:font-semibold">
