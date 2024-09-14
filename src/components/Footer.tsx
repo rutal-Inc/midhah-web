308f0a05067da7f65652ba0d5d25c6d6f0d761c9
Saad <iamsaad640@gmail.com>
2024-09-15 03:57:42 +0500

    Redesign footer, google play badge added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,74 +1,102 @@
+"use client";
+
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { promoLines } from "../utilities/constants";
+import GooglePlayBadge from "./GooglePlayBadge";
 
 function Footer() {
   const today = new Date();
   const year = today.getFullYear();
-  return (
-    <div className="mx-auto flex w-[85%] flex-col items-center justify-between border-t-2 pb-10 pt-6 text-center md:flex-row">
-      <ul className="nav col- nd:justify-start flex justify-center gap-5  ">
-        <li>
-          <a
-            aria-label="Follow us on Facebook"
-            className="text-gray-500"
-            href="https://www.facebook.com/midhah.official"
-            target="_blank"
-          >
-            <i className="bi bi-facebook"></i>
-          </a>
-        </li>
-        <li className="ms-3">
-          <a
-            aria-label="Follow us on Twitter"
-            className="text-gray-500"
-            href="https://twitter.com/midhahOfficial"
-            target="_blank"
-          >
-            <i className="bi bi-twitter-x"></i>
-          </a>
-        </li>
-        <li className="ms-3">
-          <a
-            aria-label="Follow us on Instagram"
-            className="text-gray-500"
-            href="https://www.instagram.com/midhah.official/"
-            target="_blank"
-          >
-            <i className="bi bi-instagram"></i>
-          </a>
-        </li>
-        <li className="ms-3">
-          <a
-            aria-label="Checkout our source code"
-            className="text-gray-500"
-            href="https://github.com/rutal-Inc/midhah-web"
-            target="_blank"
-          >
-            <i className="bi bi-github"></i>
-          </a>
-        </li>
-      </ul>
+  const [selectedPromo, setSelectedPromo] = useState<string>("");
 
-      <div className="col-md-4 my-3 flex items-center justify-center">
-        <Image
-          src="/images/midhah-logo-grey.svg"
-          alt="midhah-logo-grey"
-          className="mr-1"
-          width={30}
-          height={30}
-        />
-        <span className="mx-1 mb-0 text-gray-500">&copy; {year} Midhah</span>
+  useEffect(() => {
+    const randomLine =
+      promoLines[Math.floor(Math.random() * promoLines.length)];
+    setSelectedPromo(randomLine);
+  }, []);
+
+  return (
+    <div className="mx-auto w-[90%] border-t-2 py-8 text-center">
+      <div className="flex flex-col items-center gap-3">
+        <h3 className="text-lg text-gray-500">{selectedPromo}</h3>
+
+        <GooglePlayBadge location="footer" />
+
+        <ul className="mt-2 flex justify-center gap-12">
+          <li>
+            <a
+              aria-label="Follow us on Facebook"
+              className="text-gray-500"
+              href="https://www.facebook.com/midhah.official"
+              target="_blank"
+            >
+              <i className="bi bi-facebook text-2xl"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Follow us on Twitter"
+              className="text-gray-500"
+              href="https://twitter.com/midhahOfficial"
+              target="_blank"
+            >
+              <i className="bi bi-twitter-x text-2xl"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Follow us on Instagram"
+              className="text-gray-500"
+              href="https://www.instagram.com/midhah.official/"
+              target="_blank"
+            >
+              <i className="bi bi-instagram text-2xl"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Checkout our source code"
+              className="text-gray-500"
+              href="https://github.com/rutal-Inc/midhah-web"
+              target="_blank"
+            >
+              <i className="bi bi-github text-2xl"></i>
+            </a>
+          </li>
+        </ul>
       </div>
 
-      <div className="col-md-4 order-md-3 order-2 my-3 flex justify-center md:justify-end">
-        <Link
-          href="/privacy-policy"
-          className="text-de text-gray-500"
-          style={{ textDecoration: "none" }}
-        >
-          Privacy Policy
-        </Link>
+      <hr className="my-8 border-gray-300" />
+
+      <div className="flex-md-row flex flex-row items-center justify-between">
+        <div className="flex items-center">
+          <Image
+            src="/images/midhah-logo-grey.svg"
+            alt="midhah-logo-grey"
+            width={30}
+            height={30}
+            className="mr-1"
+          />
+          <span className="text-gray-500">
+            &copy; {year} Midhah{" "}
+            <span className="hidden md:inline-block">
+              Lyrics. All rights reserved
+            </span>
+          </span>
+        </div>
+
+        <div className="flex space-x-5">
+          <Link
+            href="/privacy-policy"
+            className="text-gray-500"
+            style={{ textDecoration: "none" }}
+          >
+            Privacy Policy
+          </Link>
+        </div>
       </div>
     </div>
   );
