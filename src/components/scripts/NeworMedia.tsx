e4d124c470623649bb5e334314a8af35635b8f79
Talha Meer <talha@rutal.net>
2025-04-10 17:18:26 +0500

    fix: ensure async loading for NeworMedia script

--- src/components/scripts/NeworMedia.tsx
+++ src/components/scripts/NeworMedia.tsx
@@ -5,6 +5,7 @@ function NeworMedia() {
     // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
     <Script
       strategy="beforeInteractive"
+      async
       src="//cdn.thisiswaldo.com/static/js/25508.js"
     />
   );
