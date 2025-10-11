21b4ab44d62a2158f24631a146570ab1728aa693
Talha Meer <talha@rutal.net>
2025-10-11 19:13:52 +0500

    add poet page view counter

--- src/app/poets/[slug]/page.tsx
+++ src/app/poets/[slug]/page.tsx
@@ -1,13 +1,15 @@
 import RenderLyricsList from "@/src/components/RenderLyricsList";
+import ViewCount from "@/src/components/ViewCount";
 import { WEB_BASE_URL } from "@/src/utilities/constants";
 import { capitalize } from "@/src/utilities/helpers";
 import { Metadata } from "next";
+import { headers } from "next/headers";
 import { notFound } from "next/navigation";
 
 export async function generateMetadata(props: Params): Promise<Metadata> {
   const params = await props.params;
-  const poetInfo = await searchPoet(params.slug);
-  const poetName = poetInfo!.name;
+  const poet = await fetchPoet(params.slug);
+  const poetName = poet!.name;
 
   const title = `${capitalize(
     poetName,
@@ -35,13 +37,16 @@ export async function generateMetadata(props: Params): Promise<Metadata> {
 type Params = {
   readonly params: Promise<{ slug: string }>;
 };
-export default async function GenreListPage(props: Params) {
+export default async function PoetLyricsPage(props: Params) {
   const params = await props.params;
   const slug = params.slug;
 
-  const poetInfo = await searchPoet(params.slug);
+  const poet = await fetchPoet(params.slug);
 
-  if (!poetInfo) {
+  const headersList = headers();
+  const referer = (await headersList).get("referer");
+
+  if (!poet) {
     notFound();
   }
 
@@ -49,33 +54,39 @@ export default async function GenreListPage(props: Params) {
     <div className="container mx-auto w-full md:w-[85%]">
       <div className="hero-bg card relative mb-5 overflow-hidden md:rounded-[10px]">
         <div className="py-[60px] text-center md:py-[150px]">
-          <h1 className="mb-1 text-2xl text-white md:text-5xl">
-            {poetInfo.name}
-          </h1>
+          <h1 className="mb-1 text-2xl text-white md:text-5xl">{poet.name}</h1>
         </div>
       </div>
 
       <RenderLyricsList slug={slug} type="poet" />
+
+      <ViewCount
+        entityId={poet.id}
+        entityType="LYRICS"
+        referer={`${referer}`}
+      />
     </div>
   );
 }
 
-async function searchPoet(slug: string): Promise<{ name: string } | null> {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/poets/${slug}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        cache: "no-store",
+async function fetchPoet(
+  slug: string,
+): Promise<{ id: number; name: string } | null> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/poets/${slug}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
       },
-    ).then((response) => {
-      return response.json();
-    });
+      cache: "no-store",
+    },
+  ).then((response) => {
+    return response.json();
+  });
 
-    if (res.data) {
-      return res.data;
-    }
-    return null;
+  if (res.data) {
+    return res.data;
+  }
+  return null;
 }
